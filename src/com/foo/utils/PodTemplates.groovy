0dd13cc7e7342cbac0400ad0e92a3e0403d1f648
jameshong76 <dudgh00@me.com>
2020-07-15 16:57:51 +0900

    Update PodTemplates.groovy

--- src/com/foo/utils/PodTemplates.groovy
+++ src/com/foo/utils/PodTemplates.groovy
@@ -1,7 +1,7 @@
 package com.foo.utils
 
 public void dockerTemplate(body) {
-  podTemplate(
+  podTemplate(label: 'builder',
         containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     body.call()
